29c1bb2171c59e9312ec311762784cb953b611f1
Jose Orantes <jmorante@ucsc.edu>
2023-12-08 20:50:56 -0800

    Implemented cursor sprites

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -3,11 +3,47 @@ class play extends Phaser.Scene {
         super("playScene");
     }
     preload(){
+        this.load.image('glass', './assets/glass.png');
+        this.load.image('chest', './assets/chest.png');
+        this.load.image('shovel', './assets/shovel.jpg');
+    }
 
-    }
+
     create(){
-        this.add.rectangle(0, 0, 700, 150, 0XF5F5DC).setOrigin(0,0);
+        var onSprite = false;
+        const rect = this.add.rectangle(0, 0, 700, 150, 0XF5F5DC).setOrigin(0,0).setInteractive();
+        const digArea = this.add.rectangle(350,375,700, 450, 0XEED9C4).setOrigin(0.5).setInteractive()
+
+
+        const sprite = this.add.sprite(650, 75, 'chest').setInteractive({cursor: "url(assets/glass.png), pointer"});
+        this.input.setDefaultCursor('auto');
+
+        const cursorSprite = this.add.sprite(0, 0, 'shovel'); // Use shovel as the cursor sprite
+        cursorSprite.setVisible(false);
+
+        sprite.on('pointerover', function (event){
+            onSprite = true;
+            cursorSprite.setVisible(false);
+            this.setTint(0xff0000);
+
+        });
+
+        sprite.on('pointerout', function (event){
+            onSprite = false;
+            this.clearTint();
+
+        });
+
+        this.input.on('pointermove', function (pointer) {
+            if (digArea.getBounds().contains(pointer.x, pointer.y) && onSprite == false) {
+                cursorSprite.setPosition(pointer.x, pointer.y);
+                cursorSprite.setVisible(true)
+            } else {
+                cursorSprite.setVisible(false);
+            }
+        });
     }
+
     update(){
         
     }
